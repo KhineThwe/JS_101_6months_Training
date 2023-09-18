605e9c6f313e194abb448d873eef79aca2b969aa
Khine <khinezar70122107@gmail.com>
2023-09-17 20:01:21 -1200

    sep 17 data types

--- Week12_Js/DataTypes/1.string_interpolation.js
+++ Week12_Js/DataTypes/1.string_interpolation.js
@@ -2,3 +2,4 @@ const name = "Harry";
 const age = 25;
 const result = `Results are ${name} : ${age}`;
 console.log(result);
+//Standard Output
---
+++ Week12_Js/DataTypes/2.dataTypes.js
@@ -0,0 +1,22 @@
+let blood: char = "A"; //char
+
+
+Number-- > int, Float
+Integer or Signed Integer  --> positive int , negative int
+
+Unsigned Integer --> positive integer only
+8bit, 16bit, 32bit, 64bit
+
+let num1: i8 = 100;
+
+For Signed integer: from - (2 power n - 1) to 2 power n - 1 - 1 
+For 8bit integer: From - 128 to 127
+    > OverFlow Error
+For 32 bit integer: From - 2147483648 to 2147483647
+-------------------------------------------------------------
+    For Number,
+    1.Binary
+2.Octal
+3.Hexadecimal 
+        4.Decimal
+
