26434db8d123e200d7fefd60497fc5b2852a4d97
Khine <khinezar70122107@gmail.com>
2023-09-17 18:43:02 -1200

    first commit

---
+++ Week12_Js/Variable/1.js
@@ -0,0 +1,12 @@
+// Variable
+//Vary able
+//container ---> memory
+//variable declare
+//keyword name ---> declaration
+//assignment
+//syntax
+var age; //declaration
+age = 25; //assignment
+//standard output
+console.log(age);
+console.log("Hello World");
