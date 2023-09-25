02bf58187bcbedc801dc41769b245f42ed2a1aed
Khine <khinezar70122107@gmail.com>
2023-09-24 18:54:01 -1200

    sep 24

---
+++ Week13/1.differentTypeOfstandardInput.js
@@ -0,0 +1,25 @@
+// console.log("Hello,World");
+//1.console.log()
+//2.alert()
+// alert("Hello world");
+
+//3.prompt()//Standard input function
+/*
+var username = prompt("What is username");
+
+alert(username);
+*/
+
+//4.confirm()
+// var status = confirm("Are you sure to proceed?");
+// console.log(status);
+
+// //5.document.write()
+// document.write("Hello");
+
+//6.document.getElementById()//event handler
+
+//7.`` Back tick
+
+let num = 10;
+console.log(`The num is ${num}`);
---
+++ Week13/4.pseudocode.js
@@ -0,0 +1,13 @@
+//Statically Type Language
+//define eg:Java
+
+//string username = "Khine"
+
+//Dynamically Type Language//flexible
+var username = 123;
+console.log(username);
+console.log("First Time Type Checking: " + typeof username);
+
+username = "Khine";
+console.log(username);
+console.log("Second time type checking: " + typeof username);
