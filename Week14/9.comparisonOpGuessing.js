b371efb774137361dbdd3d11dd4be5ecabca6f9e
Khine <khinezar70122107@gmail.com>
2023-09-30 19:57:26 -1200

    arithmetic op exercise,comparision op,if_else intro

---
+++ Week14/9.comparisonOpGuessing.js
@@ -0,0 +1,20 @@
+let x = 5;
+let y = 10;
+
+// Equality (==) checks if two values are equal
+console.log(x == y); //false
+
+// Inequality (!=) checks if two values are not equal
+console.log(x != y); //true
+
+// Greater than (>) checks if x is greater than y
+console.log(x > y); //false
+
+// Less than (<) checks if x is less than y
+console.log(x < y); //true
+
+// Greater than or equal to (>=) checks if x is greater than or equal to y
+console.log(x >= y); //false
+
+// Less than or equal to (<=) checks if x is less than or equal to y
+console.log(x <= y); //true
---
+++ Week14/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log("You are allowed to drink alcohol!");
