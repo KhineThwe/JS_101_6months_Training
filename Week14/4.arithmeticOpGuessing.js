02bb8fd403a4a07debfcb2bfb39dc92694964b94
Khine <khinezar70122107@gmail.com>
2023-09-30 19:00:38 -1200

    sep 30

---
+++ Week14/4.arithmeticOpGuessing.js
@@ -0,0 +1,6 @@
+console.log(1 + 2); //3
+console.log(NaN + 1); //NaN
+console.log(true + 2); //3
+console.log(false + 1); //1
+console.log("1" + 2); //12
+console.log("Hello " + "Geek"); //Hello Geek
