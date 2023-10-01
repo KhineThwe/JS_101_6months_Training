c9bd7560b79f2f63308191e22d303c022a8062a0
Khine <khinezar70122107@gmail.com>
2023-09-30 19:31:54 -1200

    sep 30 comparison operator

---
+++ Week14/6.comparisonOp.js
@@ -0,0 +1,21 @@
+/*
+Comparison Operator: return boolean data type for eg: true or false
+1. ==  Equal 
+2. ===  Equal Value and type(strict operator)
+3.!=   Not equal
+4.!== Not equal value and type 
+5.>    Greater Than
+6.<    Less Than
+7.>=   Greater than or equal 
+8.<=   Less than or equal
+ */
+let a = 5;
+let b = 2;
+let c = "hello";
+
+console.log(a == 5); //true
+console.log(b == "2"); // true
+console.log(b === "2"); //false
+console.log(c == "Hello"); //false
+
+//equal operator
---
+++ Week14/7.comparisonOp.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 2;
+let c = "hello";
+
+console.log(a != 2); //true
+console.log(b != "2"); //false
+console.log(b !== "2"); // true
+console.log(c != "Hello"); //true
+
+// not equal operator
