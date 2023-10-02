b6468899f7da1baf48f46e56ebb185ba6d6c1318
Khine <khinezar70122107@gmail.com>
2023-10-01 19:54:40 -1200

    october 1 if else if else

---
+++ Week14/14.ifStatement.js
@@ -0,0 +1,12 @@
+var age = 25;
+if (age >= 18) {
+  console.log("You are adult.");
+} else if (age >= 25) {
+  console.log("You are mid-level.");
+} else if (age >= 40) {
+  console.log("You are woman now");
+} else if (age >= 60) {
+  console.log("You can retire now");
+} else {
+  console.log("You are a minor.");
+}
