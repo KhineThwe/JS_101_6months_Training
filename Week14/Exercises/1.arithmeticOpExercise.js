762f0efff037012632022ff7bec25163a421e79a
Khine <khinezar70122107@gmail.com>
2023-10-01 18:25:47 -1200

    october 1 exercises

---
+++ Week14/Exercises/1.arithmeticOpExercise.js
@@ -0,0 +1,9 @@
+var length = 5;
+var width = 3;
+var rectangleArea = length * width;
+console.log("The are of the rectangle is : " + rectangleArea);
+
+var radius = 5;
+var circleArea = Math.PI * Math.pow(radius, 2);
+console.log(Math.PI);
+console.log(`The area of the circle is : ${circleArea}`);
---
+++ Week14/Exercises/2.comparisonOpExercise.js
@@ -0,0 +1,9 @@
+var num1 = 1;
+var num2 = 5;
+if (num1 >= num2) {
+  // 5 >= 5
+  //1 >= 5 --> false
+  console.log(num1 + " is greater than or equal to " + num2);
+} else {
+  console.log(num1 + " is not greater than or equal to " + num2);
+}
