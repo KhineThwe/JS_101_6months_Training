e03a61078f62692f8f09affbadd7d090d41708e0
khinezarthwe <khinezar70122107@gmail.com>
2023-11-26 18:40:35 -1200

    inheritance modified

--- Week21/OOP/8.inheritanceLesson.js
+++ Week21/OOP/8.inheritanceLesson.js
@@ -1,25 +1,55 @@
-class Enemy {
+class Character {
+  constructor(speed) {
+    this.speed = speed;
+  }
+  move = () => console.log(`I'm moving at the speed of ${this.speed}`);
+}
+class Enemy extends Character {
   //parent class or base class
-  constructor(power) {
+  constructor(name, phrase, power, speed) {
+    super(speed);
+    this.name = name;
+    this.phrase = phrase;
     this.power = power;
   }
+  sayPhrase = () => console.log(this.phrase);
   attack = () => console.log(`I'm attacking with a power of ${this.power}v !`);
 }
 
 class Alien extends Enemy {
   //inheritance // child class or derived class
-  constructor(name, phrase, power) {
-    super(power);
-    this.name = name;
-    this.phrase = phrase;
+  constructor(name, phrase, power, speed) {
+    super(name, phrase, speed, power);
     this.species = "alien";
   }
   fly = () => console.log("Fly");
-  sayPhrase = () => console.log(this.phrase);
 }
 
-const alien1 = new Alien("Ali", "I am all alien", 10);
-alien1.attack();
+class Bug extends Enemy {
+  constructor(name, phrase, power, speed) {
+    super(name, phrase, power, speed);
+    this.species = "bug";
+  }
+  hide = () => console.log("You can't catch me now!");
+}
 
-const enemy = new Enemy(100);
-enemy.attack();
+class Robot extends Enemy {
+  constructor(name, phrase, power, speed) {
+    super(name, phrase, power, speed);
+    this.species = "robot";
+  }
+  transform = () => console.log("Optimus prime!");
+}
+
+const alien1 = new Alien("Ali", "I am all alien", 10, 50);
+const alien2 = new Alien("Lien", "Run for your lives!", 15, 60);
+
+const bug1 = new Bug("Buggy", "Your debugger doesn't work with me!", 25, 100);
+const bug2 = new Bug("Erik", "I drink decaf!", 5, 120);
+
+const Robot1 = new Robot("Tito", "I can cook,swim and dance!", 125, 30);
+const Robot2 = new Robot("Terminator", "I can cook", 155, 400);
+
+alien1.attack();
+alien2.attack();
+bug1.move();
